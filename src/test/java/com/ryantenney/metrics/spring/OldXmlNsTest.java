6b89ab3143c596a30343f3f5ae57c797c3e151bb
Ryan W Tenney <ryan@10e.us>
2012-06-18 00:13:44 -0400

    Fixing support for old www.yammer.com xmlns.

---
+++ src/test/java/com/ryantenney/metrics/spring/OldXmlNsTest.java
@@ -0,0 +1,21 @@
+package com.ryantenney.metrics.spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yammer.metrics.HealthChecks;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.HealthCheckRegistry;
+import com.yammer.metrics.core.MetricsRegistry;
+
+public class OldXmlNsTest {
+
+	@Test
+	public void testOldXmlNs() {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:old-xmlns.xml");
+		Assert.assertSame("Should be default MetricsRegistry.", Metrics.defaultRegistry(), ctx.getBean(MetricsRegistry.class));
+		Assert.assertSame("Should be default HealthCheckRegistry.", HealthChecks.defaultRegistry(), ctx.getBean(HealthCheckRegistry.class));
+	}
+
+}
