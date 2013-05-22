61573979f6b4ee709881853fabaabe3c4e1d0f60
thyzz <vonk.thijs@gmail.com>
2013-05-22 21:31:59 +0200

    Added a new MethodKey to be used in the HashMap. within the Interceptors to identify the called methods

---
+++ src/main/java/com/ryantenney/metrics/spring/MethodKey.java
@@ -0,0 +1,62 @@
+package com.ryantenney.metrics.spring;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class MethodKey {
+  private final String name;
+  private final Class<?> returnType;
+  private final Class<?>[] parameterTypes;
+  private final Class<?>[] exceptionTypes;
+
+  public static MethodKey forMethod(Method method) {
+    return new MethodKey(method);
+  }
+    
+  private MethodKey(Method method){
+    this.name = method.getName();
+    this.returnType = method.getReturnType();
+    this.parameterTypes = method.getParameterTypes();
+    this.exceptionTypes = method.getExceptionTypes();
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + Arrays.hashCode(exceptionTypes);
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + Arrays.hashCode(parameterTypes);
+    result = prime * result + ((returnType == null) ? 0 : returnType.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    MethodKey other = (MethodKey) obj;
+    if (!Arrays.equals(exceptionTypes, other.exceptionTypes))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (!Arrays.equals(parameterTypes, other.parameterTypes))
+      return false;
+    if (returnType == null) {
+      if (other.returnType != null)
+        return false;
+    } else if (!returnType.equals(other.returnType))
+      return false;
+    return true;
+  }
+  
+  
+
+}
