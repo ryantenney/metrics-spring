11c720a02ef196ca99f10966def916307eb78859
Ryan Tenney <ryan@10e.us>
2013-09-03 14:12:11 -0400

    Dedupe much logic from the method interceptors

---
+++ src/main/java/com/ryantenney/metrics/spring/AbstractMetricMethodInterceptor.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2012 Ryan W Tenney (http://ryan.10e.us)
+ *            and Martello Technologies (http://martellotech.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ryantenney.metrics.spring;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.ReflectionUtils.MethodCallback;
+import org.springframework.util.ReflectionUtils.MethodFilter;
+
+import com.codahale.metrics.MetricRegistry;
+
+abstract class AbstractMetricMethodInterceptor<A extends Annotation, M> implements MethodInterceptor, MethodCallback {
+
+	protected final Logger LOG = LoggerFactory.getLogger(getClass());
+
+	protected final MetricRegistry metricRegistry;
+	protected final Class<?> targetClass;
+	protected final Class<A> annotationClass;
+	protected final Map<MethodKey, M> metrics;
+
+	AbstractMetricMethodInterceptor(final MetricRegistry metricRegistry, final Class<?> targetClass, final Class<A> annotationClass, final MethodFilter methodFilter) {
+		this.metricRegistry = metricRegistry;
+		this.targetClass = targetClass;
+		this.annotationClass = annotationClass;
+		this.metrics = new HashMap<MethodKey, M>();
+
+		LOG.debug("Creating method interceptor for class {}", targetClass.getCanonicalName());
+		LOG.debug("Scanning for @{} annotated methods", annotationClass.getSimpleName());
+
+		ReflectionUtils.doWithMethods(targetClass, this, methodFilter);
+	}
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		final M metric = metrics.get(MethodKey.forMethod(invocation.getMethod()));
+		if (metric != null) {
+			return invoke(invocation, metric);
+		}
+		else {
+			return invocation.proceed();
+		}
+	}
+
+	@Override
+	public void doWith(Method method) throws IllegalAccessException {
+		final A annotation = method.getAnnotation(annotationClass);
+		final MethodKey methodKey = MethodKey.forMethod(method);
+		final String metricName = buildMetricName(targetClass, method, annotation);
+		final M metric = buildMetric(metricRegistry, metricName, annotation);
+
+		metrics.put(methodKey, metric);
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("Created {} {} for method {}", metric.getClass().getSimpleName(), metricName, methodKey);
+		}
+	}
+
+	protected abstract String buildMetricName(Class<?> targetClass, Method method, A annotation);
+
+	protected abstract M buildMetric(MetricRegistry metricRegistry, String metricName, A annotation);
+
+	protected abstract Object invoke(MethodInvocation invocation, M metric) throws Throwable;
+
+}
--- src/main/java/com/ryantenney/metrics/spring/TimedMethodInterceptor.java
+++ src/main/java/com/ryantenney/metrics/spring/TimedMethodInterceptor.java
@@ -17,75 +17,62 @@
 package com.ryantenney.metrics.spring;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.aop.Advice;
 import org.aopalliance.intercept.MethodInvocation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.core.Ordered;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.util.ReflectionUtils.MethodCallback;
 import org.springframework.util.ReflectionUtils.MethodFilter;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
+import com.codahale.metrics.Timer.Context;
 import com.codahale.metrics.annotation.Timed;
 
-class TimedMethodInterceptor implements MethodInterceptor, MethodCallback, Ordered {
+class TimedMethodInterceptor extends AbstractMetricMethodInterceptor<Timed, Timer> implements Ordered {
 
-	private static final Logger LOG = LoggerFactory.getLogger(TimedMethodInterceptor.class);
+	public static final Class<Timed> ANNOTATION = Timed.class;
+	public static final Pointcut POINTCUT = new AnnotationMatchingPointcut(null, ANNOTATION);
+	public static final MethodFilter METHOD_FILTER = new AnnotationFilter(ANNOTATION);
 
-	public static final Pointcut POINTCUT = new AnnotationMatchingPointcut(null, Timed.class);
-	public static final MethodFilter METHOD_FILTER = new AnnotationFilter(Timed.class);
-
-	private final MetricRegistry metrics;
-	private final Class<?> targetClass;
-	private final Map<MethodKey, Timer> timers;
-
-	public TimedMethodInterceptor(final MetricRegistry metrics, final Class<?> targetClass) {
-		this.metrics = metrics;
-		this.targetClass = targetClass;
-		this.timers = new HashMap<MethodKey, Timer>();
-
-		LOG.debug("Creating method interceptor for class {}", targetClass.getCanonicalName());
-		LOG.debug("Scanning for @Timed annotated methods");
-
-		ReflectionUtils.doWithMethods(targetClass, this, METHOD_FILTER);
+	public TimedMethodInterceptor(final MetricRegistry metricRegistry, final Class<?> targetClass) {
+		super(metricRegistry, targetClass, ANNOTATION, METHOD_FILTER);
 	}
 
 	@Override
-	public Object invoke(MethodInvocation invocation) throws Throwable {
-		final Timer timer = timers.get(MethodKey.forMethod(invocation.getMethod()));
-		final com.codahale.metrics.Timer.Context timerCtx = timer != null ? timer.time() : null;
+	protected Object invoke(MethodInvocation invocation, Timer timer) throws Throwable {
+		final Context timerCtx = timer.time();
 		try {
 			return invocation.proceed();
 		}
 		finally {
-			if (timerCtx != null) {
-				timerCtx.close();
-			}
+			timerCtx.close();
 		}
 	}
 
 	@Override
-	public void doWith(Method method) throws IllegalAccessException {
-		final Timed annotation = method.getAnnotation(Timed.class);
-		final String metricName = Util.forTimedMethod(targetClass, method, annotation);
-		final MethodKey methodKey = MethodKey.forMethod(method);
-		final Timer timer = metrics.timer(metricName);
+	protected Timer buildMetric(MetricRegistry metricRegistry, String metricName, Timed annotation) {
+		return metricRegistry.timer(metricName);
+	}
 
-		timers.put(methodKey, timer);
-
-		LOG.debug("Created Timer {} for method {}", metricName, methodKey);
+	@Override
+	protected String buildMetricName(Class<?> targetClass, Method method, Timed annotation) {
+		return Util.forTimedMethod(targetClass, method, annotation);
 	}
 
 	@Override
 	public int getOrder() {
 		return HIGHEST_PRECEDENCE;
+	}
+
+	static AdviceFactory adviceFactory(final MetricRegistry metricRegistry) {
+		return new AdviceFactory() {
+			@Override
+			public Advice getAdvice(Object bean, Class<?> targetClass) {
+				return new TimedMethodInterceptor(metricRegistry, targetClass);
+			}
+		};
 	}
 
 }
