7baf4398074c8814de0cb34e5678a41609510c28
Ryan Tenney <ryan@10e.us>
2013-09-04 17:46:24 -0400

    Add maven-license-plugin and update headers

--- src/main/java/com/ryantenney/metrics/annotation/InjectMetric.java
+++ src/main/java/com/ryantenney/metrics/annotation/InjectMetric.java
@@ -1,12 +1,11 @@
-/*
- * Copyright 2012 Ryan W Tenney (http://ryan.10e.us)
- *            and Martello Technologies (http://martellotech.com)
+/**
+ * Copyright (C) 2012 Ryan W Tenney (ryan@10e.us)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
