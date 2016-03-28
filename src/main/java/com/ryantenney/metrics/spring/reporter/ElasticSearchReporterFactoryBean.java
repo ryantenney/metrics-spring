c0036f691dfee31b24e04dd8db58d9ca53ab5d08
david <david@davidkarlsen.com>
2016-03-28 22:24:45 +0200

    Fix docs and licences

--- src/main/java/com/ryantenney/metrics/spring/reporter/ElasticSearchReporterFactoryBean.java
+++ src/main/java/com/ryantenney/metrics/spring/reporter/ElasticSearchReporterFactoryBean.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2012 Ryan W Tenney (ryan@10e.us)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ryantenney.metrics.spring.reporter;
 
 import org.elasticsearch.metrics.ElasticsearchReporter;
@@ -8,7 +23,6 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Configuration of Metrics reporter using ES.
- * Done in @Configuration since spring xml has no good support for fluent beans.
  *
  * @author <a href="mailto:david@davidkarlsen.com>David J. M. Karlsen</a>
  * @since 3.1.3
@@ -31,7 +45,7 @@ public class ElasticSearchReporterFactoryBean
             .additionalFields(getPropertyRef(ElasticSearchReporterElementParser.ADDITIONAL_FIELDS_REF, Map.class))
             .prefixedWith(getPrefix())
             .filter(getMetricFilter())
-            .hosts(getProperty( ElasticSearchReporterElementParser.HOSTS, String[].class ))
+            .hosts(getProperty(ElasticSearchReporterElementParser.HOSTS, String[].class))
             .build();
 
         elasticsearchReporter.start(getPeriod(), TimeUnit.NANOSECONDS);
